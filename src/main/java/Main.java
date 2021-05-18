eabc6f6c774a432943646ea8d5fb5aeb2166603d
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2021-05-18 16:51:42 +0200

    DateFormat eingebaut

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,31 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 public class Main {
     public static void main(String[] args){
         System.out.printf(GetMessage());
 
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String strDate = formatter.format(date);
+        System.out.println("Date Format with MM/dd/yyyy : "+strDate);
+
+        formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd-M-yyyy hh:mm:ss : "+strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd MMMM yyyy : "+strDate);
+
+        formatter = new SimpleDateFormat("dd MMMM yyyy zzzz");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with dd MMMM yyyy zzzz : "+strDate);
+
+        formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
+        strDate = formatter.format(date);
+        System.out.println("Date Format with E, dd MMM yyyy HH:mm:ss z : "+strDate);
 
     }
 
